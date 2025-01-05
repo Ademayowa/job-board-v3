93a47a7fde6c820ab0553ad2a22ad6109e79426c
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-05 19:00:07 +0100

    feat: added single job page

--- components/JobList.tsx
+++ components/JobList.tsx
@@ -1,21 +1,28 @@
 import { Job } from '@/types';
+import Link from 'next/link';
 
 export default function JobList({ jobs }: { jobs: Job[] }) {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
       {jobs.map((job) => (
-        <div key={job.ID} className='rounded-lg p-6 shadow-md bg-white'>
-          <h2 className='text-xl font-semibold'>{job.Title}</h2>
-          <p className='mt-2'>{job.Description}</p>
-          <div className='mt-4'>
-            <p className='text-sm font-medium'>
-              <span className='font-semibold'>Location:</span> {job.Location}
-            </p>
-            <p className='text-sm font-medium'>
-              <span className='font-semibold'>Salary:</span> ${job.Salary}
-            </p>
+        <Link
+          key={job.ID}
+          href={`/job/${job.ID}`}
+          className='rounded-lg p-6 shadow-md bg-white block'
+        >
+          <div>
+            <h2 className='text-xl font-semibold'>{job.Title}</h2>
+            <p className='mt-2'>{job.Description}</p>
+            <div className='mt-4'>
+              <p className='text-sm font-medium'>
+                <span className='font-semibold'>Location:</span> {job.Location}
+              </p>
+              <p className='text-sm font-medium'>
+                <span className='font-semibold'>Salary:</span> ${job.Salary}
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
