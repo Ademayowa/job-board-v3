0a002310ed9cb18a958fc9ef00b162c89ed9479a
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-10 15:05:47 +0100

    chore: UI updates

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -40,7 +40,7 @@ export default function SearchForm() {
 
         <input
           type='text'
-          placeholder='Search a job by title'
+          placeholder='Search a job by title E.g golang backend engineer'
           value={title}
           onChange={handleInputChange}
           className='w-full h-14 rounded-md border border-slate-500 outline-none md:border-none'
