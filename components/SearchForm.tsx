608f835f58968f0c3ec0f0867cf1ea2de3abb3ef
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-22 08:38:22 +0100

    feat: close button for clearing text search in form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -61,7 +61,7 @@ export default function SearchForm() {
           <button
             type='button'
             onClick={handleClear}
-            className='absolute right-3 text-blue-500 hover:text-gray-700 focus:outline-none !text-3xl'
+            className='absolute right-3 text-blue-500 hover:text-gray-700 focus:outline-none text-3xl'
           >
             ×
           </button>
