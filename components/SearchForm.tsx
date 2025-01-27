c773213da45bd79d7a1df1b1842f784c43848cdb
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-27 08:40:20 +0100

    fix: fixing accesibility issues on x button

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -64,7 +64,8 @@ export default function SearchForm() {
           <button
             type='button'
             onClick={handleClear}
-            className='absolute right-5 text-blue-500 hover:text-gray-700 text-2xl'
+            className='absolute right-4 w-8 h-8 text-blue-500 hover:text-gray-700 text-2xl flex !items-center justify-center'
+            aria-label='clear input'
           >
             ×
           </button>
