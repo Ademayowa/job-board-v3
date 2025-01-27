b65803bd2f0fa7b426b5195cf4f5276411f046d1
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-27 07:34:18 +0100

    fix: fixing accesibility issue on search icon

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -42,12 +42,14 @@ export default function SearchForm() {
 
   return (
     <form
-      aria-hidden='true'
       onSubmit={handleSearch}
       className='flex flex-col md:flex-row gap-3 items-center bg-white px-5 md:py-4 py-8 rounded-lg md:w-10/12 w-full'
     >
       <div className='flex items-center w-full rounded-md relative'>
-        <Search className='h-6 w-6 text-blue-500 hidden md:block mr-2' />
+        <Search
+          aria-hidden='true'
+          className='h-6 w-6 text-blue-500 hidden md:block mr-2'
+        />
 
         <input
           type='text'
