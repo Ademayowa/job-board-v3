7bb80532e86ebb41be896ed2dd5e637ec4fb292e
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-27 07:26:39 +0100

    fix: fixing arial accesibility issue

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -42,13 +42,11 @@ export default function SearchForm() {
 
   return (
     <form
+      aria-hidden='true'
       onSubmit={handleSearch}
       className='flex flex-col md:flex-row gap-3 items-center bg-white px-5 md:py-4 py-8 rounded-lg md:w-10/12 w-full'
     >
-      <div
-        aria-hidden='true'
-        className='flex items-center w-full rounded-md relative'
-      >
+      <div className='flex items-center w-full rounded-md relative'>
         <Search className='h-6 w-6 text-blue-500 hidden md:block mr-2' />
 
         <input
