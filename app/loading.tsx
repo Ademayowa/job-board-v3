4820475945298bd29fa28927d3ecef6f0f492a8f
ademayowa <ademayowaadeniyi@gmail.com>
2025-02-06 14:15:28 +0100

    chore: increase loader size

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
       <div role='status'>
         <svg
           aria-hidden='true'
-          className='inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#0F4A7B]'
+          className='inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#0F4A7B]'
           viewBox='0 0 100 101'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
@@ -18,7 +18,6 @@ export default function Loading() {
             fill='currentFill'
           />
         </svg>
-        <span className='text-[#707071] text-2xl'></span>
       </div>
     </div>
   );
